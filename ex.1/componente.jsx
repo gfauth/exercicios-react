c59f084f2a90160bcdac545cdf7be478fd81124d
LUAN GODOY FAUTH <4255795+gfauth@users.noreply.github.com>
2018-03-28 08:44:16 -0300

    commit dos exercicios de react de hoje.

---
+++ ex.1/componente.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+
+export default props => <h1>primeiro aqui</h1>;
+export const Segundo = props => <h1>segundo aqui</h1>;
---
+++ ex/componente.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default props => <h1>primeiro aqui</h1>;
+export const Segundo = props => <h1>segundo aqui</h1>;
+
